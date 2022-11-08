dbdc8d0f0fb5dde83075f3bcf0498d1c4eab70b2
Md Rajikul Islame <mdrajikul779@gmail.com>
2022-11-08 23:53:29 +0600

    delete api created

--- index.js
+++ index.js
@@ -67,6 +67,13 @@ async function run() {
         res.send(review);
     });
 
+          app.delete('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await ReviewsCollection.deleteOne(query);
+            res.send(result);
+        })
+
   } finally {
   }
 }
