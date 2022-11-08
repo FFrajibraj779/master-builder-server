f5deeb07c1351ac4051a716c69bbb66dbc9cd84f
Md Rajikul Islame <mdrajikul779@gmail.com>
2022-11-08 12:20:08 +0600

    mongodb added

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
+require('dotenv').config();
 const port = process.env.PORT || 5000 ;
 
 //middleWare
 app.use(cors())
 app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.lrjyghr.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 app.get('/', (req, res)=>{
